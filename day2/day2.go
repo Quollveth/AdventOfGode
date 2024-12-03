a0ea5e1a031b3b67b0b2f29ce16dd44df2aa2646
Quollveth <quollveth@gmail.com>
2024-12-02 23:22:20 -0300

    day 2 part 2

--- day2/day2.go
+++ day2/day2.go
@@ -15,49 +15,71 @@ func absdiff(x, y int) int {
 	return x - y
 }
 
+func removeAtIndex(slice []int, index int) []int {
+	// no bound check just dont call a oob index
+	new := make([]int, 0, len(slice)-1)
+	new = append(new, slice[:index]...)
+	new = append(new, slice[index+1:]...)
+
+	return new
+}
+
+func checkReport(report []int) bool {
+	prev := 0
+	increasing := false
+	for i, level := range report {
+		if i == 0 {
+			prev = level
+			continue
+		}
+
+		diff := absdiff(prev, level)
+
+		if diff == 0 || diff > 3 {
+			return false
+		}
+
+		if i == 1 {
+			increasing = level > prev
+
+			prev = level
+			continue
+		}
+
+		if (level > prev) != increasing {
+			return false
+		}
+
+		prev = level
+	}
+	return true
+}
+
 func Run() {
 	input := util.ReadFileLines("day2/input")
 
 	nSafe := 0
 	for _, line := range input {
-		splits := strings.Split(line, " ")
-
-		var prev int
-		increasing := true
-		safe := true
-
-		for i, v := range splits {
-			level, _ := strconv.Atoi(v)
-
-			if i == 0 {
-				prev = level
-				continue
-			}
-
-			diff := absdiff(prev, level)
-
-			if diff == 0 || diff > 3 {
-				safe = false
-				break
-			}
-
-			if i == 1 {
-				increasing = level > prev
-
-				prev = level
-				continue
-			}
-
-			if (level > prev) != increasing {
-				safe = false
-				break
-			}
-
-			prev = level
+		// convert to int array
+		var report []int
+		for _, level := range strings.Split(line, " ") {
+			v, _ := strconv.Atoi(level)
+			report = append(report, v)
 		}
 
-		if safe {
+		valid := checkReport(report)
+
+		if valid {
 			nSafe++
+			continue
+		}
+
+		// time complexity is O(shit)
+		for i := range len(report) {
+			if checkReport(removeAtIndex(report, i)) {
+				nSafe++
+				break
+			}
 		}
 	}
 
---
+++ day2/day2_test.go
@@ -0,0 +1,9 @@
+package day2
+
+import (
+	"testing"
+)
+
+func TestPuzzle(t *testing.T) {
+
+}
